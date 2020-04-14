4caefd4b235463271087e8ba36e729424ee22d79
Alec Prettyman <alecprettyman@alecs-mbp.lan>
2020-04-14 13:29:52 -0600

    lecture completed

---
+++ server/controllers/authController.js
@@ -0,0 +1,55 @@
+const bcrypt = require('bcryptjs')
+
+module.exports = {
+  register: async (req, res) => {
+    const db = req.app.get('db')
+
+    const {email, password} = req.body
+
+    const existingUser = await db.get_user_by_email([email])
+
+    if(existingUser[0]){
+      return res.status(409).send('User already exist')
+    }
+
+    const salt = bcrypt.genSaltSync(10)
+    const hash = bcrypt.hashSync(password, salt)
+
+    const newUser = await db.create_user([email, hash])
+
+    delete newUser[0].hash
+
+    req.session.user = newUser[0]
+
+    res.status(200).send(req.session.user)
+  },
+
+  login: async (req, res) => {
+    const db = req.app.get('db')
+
+    const {email, password} = req.body
+
+    const existingUser = await db.get_user_by_email([email])
+
+    if(!existingUser[0]){
+      return res.status(404).send('Email or password is incorrect')
+    }
+
+    const authenticated = bcrypt.compareSync(password, existingUser[0].hash)
+
+    if(authenticated){
+      delete existingUser[0].hash
+
+      req.session.user = existingUser[0]
+
+      res.status(200).send(req.session.user)
+    }else {
+      return res.status(401).send('Email or password is incorrect')
+    }
+  },
+
+  logout: (req, res) => {
+    req.session.destroy()
+    res.sendStatus(200)
+  }
+}
